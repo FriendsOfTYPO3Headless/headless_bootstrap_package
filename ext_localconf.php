b129439ca2364db0b91df4a19b27fa044417df52
Niels Seelhöfer <niels.seelhoefer@trixie.de>
2022-02-10 12:13:06 +0100

    Code Cleanup + Debugging + Auseinanderführen der Extensions

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,4 +2,4 @@
 
 defined('TYPO3_MODE') || die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:headless_bootstrap_package/Configuration/TSConfig/*.tsconfig"');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:headless_bootstrap_package/Configuration/TSConfig/ALL.tsconfig"');
