c78d177ac1cf27334d6018ddae7d73aba8ae08e5
Sven Harders <sven@hardanders.de>
2022-03-03 16:08:59 +0100

    added BulletsElementTest.php #30 #31

---
+++ Tests/Functional/ContentTypes/BulletsElementTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the "headless_bootstrap_package" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FriendsOfTYPO3Headless\HeadlessBootstrapPackage\Tests\Functional\ContentTypes;
+
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
+
+class BulletsElementTest extends BaseContentTypeTest
+{
+    public function testHeaderContentElement(): void
+    {
+        $response = $this->executeFrontendSubRequest(
+            new InternalRequest('https://website.local/')
+        );
+
+        self::assertEquals(200, $response->getStatusCode());
+
+        $fullTree = json_decode((string)$response->getBody(), true);
+
+        $contentElement = $fullTree['content']['colPos0'][2];
+
+        // content element specific tests
+        self::assertEquals(1, $contentElement['content']['bulletsType'], 'bulletsType mismatch');
+        self::assertEquals('1234567890', $contentElement['content']['date'], 'date mismatch');
+        self::assertCount(4, $contentElement['content']['bodytext']);
+
+        // general tests
+        $this->checkDefaultContentFields($contentElement, 3, 1, 'bullets', 0, 'SysCategory1Title,SysCategory2Title');
+        $this->checkAppearanceFields($contentElement, 'layout-1', 'Frame', 'SpaceBefore', 'SpaceAfter', 'embedded', 'primary', '1', '1');
+        $this->checkHeaderFields($contentElement, 'Header', 'Subheader', 1, 'center');
+        $this->checkBackgroundImageField($contentElement);
+        $this->checkBackgroundImageOptions($contentElement, '1', '1', 'blur');
+    }
+}
