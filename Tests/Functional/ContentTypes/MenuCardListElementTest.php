25d06998e954d6454a9738825bae377579861f3f
svenpet90 <sp@dauskonzept.de>
2022-03-07 10:02:29 +0100

    added MenuCardListElementTest.php

--- Tests/Functional/ContentTypes/MenuCardListElementTest.php
+++ Tests/Functional/ContentTypes/MenuCardListElementTest.php
@@ -13,7 +13,7 @@ namespace FriendsOfTYPO3Headless\HeadlessBootstrapPackage\Tests\Functional\Conte
 
 use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
 
-class MenuThumbnailListElementTest extends BaseContentTypeTest
+class MenuCardListElementTest extends BaseContentTypeTest
 {
     public function testHeaderContentElement(): void
     {
@@ -25,7 +25,7 @@ class MenuThumbnailListElementTest extends BaseContentTypeTest
 
         $fullTree = json_decode((string)$response->getBody(), true);
 
-        $contentElement = $fullTree['content']['colPos0'][12];
+        $contentElement = $fullTree['content']['colPos0'][13];
 
         // content element specific tests
         self::assertEquals(1, $contentElement['accessibility']['bypass']);
@@ -34,7 +34,7 @@ class MenuThumbnailListElementTest extends BaseContentTypeTest
         $this->checkItems($contentElement);
 
         // general tests
-        $this->checkDefaultContentFields($contentElement, 13, 1, 'menu_thumbnail_list', 0, 'SysCategory1Title,SysCategory2Title');
+        $this->checkDefaultContentFields($contentElement, 14, 1, 'menu_card_list', 0, 'SysCategory1Title,SysCategory2Title');
         $this->checkAppearanceFields($contentElement, 'layout-1', 'Frame', 'SpaceBefore', 'SpaceAfter', 'embedded', 'primary', '1', '1');
         $this->checkHeaderFields($contentElement, 'Header', 'Subheader', 1, 'center');
         $this->checkBackgroundImageField($contentElement);
@@ -54,7 +54,7 @@ class MenuThumbnailListElementTest extends BaseContentTypeTest
         self::assertCount(3, $contentElement['content']['items']);
 
         foreach ($contentElement['content']['items'] as $page) {
-            self::assertCount(8, $page);
+            self::assertCount(9, $page);
             self::assertArrayHasKey('title', $page);
             self::assertArrayHasKey('link', $page);
             self::assertArrayHasKey('target', $page);
@@ -62,7 +62,9 @@ class MenuThumbnailListElementTest extends BaseContentTypeTest
             self::assertArrayHasKey('current', $page);
             self::assertArrayHasKey('spacer', $page);
             self::assertArrayHasKey('hasSubpages', $page);
-            self::assertArrayHasKey('media', $page);
+            self::assertArrayHasKey('thumbnail', $page);
+            self::assertIsArray($page['thumbnail']);
+            self::assertArrayHasKey('nav_icon', $page);
         }
     }
 }
